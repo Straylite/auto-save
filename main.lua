3aa357a2fdfcbbb0033348efe246a63c524fbb3c
Straylite <49315393+Straylite@users.noreply.github.com>
2020-10-18 14:35:09 +0200

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
-local m = 2
+local minutes = 2
 local t = 0
+local ft_c = os.date("%b%d-(%Y) - %H_%M_%S")
+savestate.save("backup ".. ft_c .. ".state")
 while true do
     local ft = os.date("%b%d-(%Y) - %H_%M_%S")
     t = t + 1
-    if t > m*(60*60) then savestate.save("backup ".. ft .. ".state") t = 0 end
+    if t > minutes*(60*60) then savestate.save("backup ".. ft .. ".state") t = 0 end
     emu.frameadvance()
 end
